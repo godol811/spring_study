53063e634197d1c085fc753702fd5b63ddd8ff3b
Jongchan Ko <godol811@naver.com>
2022-08-19 23:35:55 +0900

    2022 0819 SpringDemo - 1. bean life cycle -> bean에서 메소드 이름과 함께 선언 할수 있다. init & destroy 2. prototype scope bean은 파괴가 되지 않는다. 굳이 파괴를 하고 싶으면 DisposableBean을 받아 overriding 받아 사용하면 된다.
    3. bean은 scope가 싱글톤이면 다른 변수에서 같은 bean을 불러오면 그 객체의 값은 동일하다 (값, id)

---
+++ src/main/java/springdemo/BeanScopeDemoApp.java
@@ -0,0 +1,31 @@
+package springdemo;
+
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class BeanScopeDemoApp {
+
+    public static void main(String[] args) {
+
+        // load the spring configuration file
+        ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("beanScope-applicationContext.xml");
+
+        // retrieve bean from spring container
+        Coach theCoach = context.getBean("myCoach", Coach.class);
+
+        Coach alphaCoach = context.getBean("myCoach", Coach.class);
+
+        boolean result = (theCoach == alphaCoach);
+
+        boolean result2 = (theCoach.equals(alphaCoach));
+
+
+        System.out.println("\nPointing to the same object  " +  result + "  equals  " + result2);
+        System.out.println(theCoach);
+        System.out.println(alphaCoach);
+
+        //close context
+
+        context.close();
+    }
+
+}
